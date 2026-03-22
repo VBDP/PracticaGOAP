47a6604e696e2354617cf77dd2a95ffdf0cdeb5b
vbdp <vbd@vbd.es>
2026-03-22 10:15:30 +0100

    Ahora se limita a 8 pacientes, cada vez que entran maximo 8 cuando se va uno entra uno hasta ser 8

--- Assets/Scripts/GoHome.cs
+++ Assets/Scripts/GoHome.cs
@@ -1,4 +1,10 @@
 ﻿public class GoHome : GAction {
+    public Spawn spawn;
+
+    void Start()
+    {
+        spawn = FindFirstObjectByType<Spawn>();
+    }
     public override bool PrePerform() {
 
         return true;
@@ -7,6 +13,8 @@
     public override bool PostPerform() {
 
         Destroy(this.gameObject);
+        spawn.RestarPaciente();
         return true;
+        
     }
 }
--- Assets/Scripts/Spawn.cs
+++ Assets/Scripts/Spawn.cs
@@ -1,33 +1,45 @@
 ﻿using UnityEngine;
 
-public class Spawn : MonoBehaviour {
+public class Spawn : MonoBehaviour
+{
 
     // Grab our prefab
     public GameObject patientPrefab;
     // Number of patients to spawn
     public int numPatients;
+    public int activePacients;
+    public int maxPatients = 8;
 
-    void Start() {
-
+    void Start()
+    {
         //for (int i = 0; i < numPatients; ++i) {
 
         //    // Instantiate numPatients at the spawner
         //    Instantiate(patientPrefab, this.transform.position, Quaternion.identity);
         //}
         // Call the SpawnPatient method for the first time
+        activePacients = 0;
         Invoke("SpawnPatient", 5.0f);
     }
 
-    void SpawnPatient() {
-
+    void SpawnPatient()
+    {
         // Instantiate numPatients at the spawner
-        Instantiate(patientPrefab, this.transform.position, Quaternion.identity);
-        // Invoke this method at random intervals
-        Invoke("SpawnPatient", Random.Range(2.0f, 10.0f));
+        if (activePacients < maxPatients)
+        {
+            Instantiate(patientPrefab, this.transform.position, Quaternion.identity);
+            // Invoke this method at random intervals
+            Invoke("SpawnPatient", Random.Range(2.0f, 10.0f));
+            activePacients += 1;
+        }
+        else
+        {
+           Invoke("SpawnPatient", Random.Range(2.0f, 10.0f)); 
+        }      
     }
 
-    // Update is called once per frame
-    void Update() {
-
+    public void RestarPaciente()
+    {
+        activePacients -= 1;
     }
 }
